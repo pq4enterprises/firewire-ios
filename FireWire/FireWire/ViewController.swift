90ef8c67129906acf01ce50d84325e49401a0cc3
sujitha <atom.sujitha@gmail.com>
2024-11-12 19:18:03 +0530

    init repo, create UI for splash and login

---
+++ FireWire/FireWire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FireWire
+//
+//  Created by Sujitha Palanisamy on 12/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
