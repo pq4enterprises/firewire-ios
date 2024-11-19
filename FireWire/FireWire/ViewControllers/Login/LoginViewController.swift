a601468a624943cb252566b5ac4d0f23c97db1c8
Sujitha <atom.sujitha@gmail.com>
2024-11-20 01:42:53 +0530

    handle keyboard hide/show functionality

--- FireWire/FireWire/ViewControllers/Login/LoginViewController.swift
+++ FireWire/FireWire/ViewControllers/Login/LoginViewController.swift
@@ -11,6 +11,7 @@ class LoginViewController: UIViewController {
 
     weak var coordinator: LoginCoordinator?
 
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var registerLabel: UILabel!
     @IBOutlet weak var termsAndConditionsLabel: UILabel!
 
@@ -18,6 +19,7 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
         setupUI()
         setupActions()
+        setupKeyboardActions()
     }
 
     func setupUI(){
@@ -43,6 +45,38 @@ class LoginViewController: UIViewController {
 
     @IBAction func signInTap(_ sender: UIButton) {
         coordinator?.navigateToHome()
+    }
+
+    //TODO: Handle in common place
+    func setupKeyboardActions(){
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+
+    @objc func keyboardWillShow(notification: NSNotification) {
+        guard let userInfo = notification.userInfo,
+              let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+
+        // Calculate the inset of the scroll view
+        let keyboardHeight = keyboardFrame.height
+
+        // Set the content inset for the scroll view
+        var contentInset = scrollView.contentInset
+        contentInset.bottom = keyboardHeight
+        scrollView.contentInset = contentInset
+
+        // Adjust the scroll indicator inset
+        scrollView.scrollIndicatorInsets = contentInset
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        // Reset the content inset when the keyboard hides
+        var contentInset = scrollView.contentInset
+        contentInset.bottom = 0
+        scrollView.contentInset = contentInset
+
+        // Reset the scroll indicator inset
+        scrollView.scrollIndicatorInsets = contentInset
     }
 
     // A convenience method to instantiate from the storyboard
--- FireWire/FireWire/ViewControllers/Login/RegistrationViewController.swift
+++ FireWire/FireWire/ViewControllers/Login/RegistrationViewController.swift
@@ -11,11 +11,13 @@ class RegistrationViewController: UIViewController {
 
     var coordinator: LoginCoordinator?
 
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var signInLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        setupKeyboardActions()
     }
 
     func setupUI(){
@@ -23,6 +25,38 @@ class RegistrationViewController: UIViewController {
             text: .Register.signInText,
             coloredText: .Register.signIn
         )
+    }
+
+    //TODO: Handle in common place
+    func setupKeyboardActions(){
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+
+    @objc func keyboardWillShow(notification: NSNotification) {
+        guard let userInfo = notification.userInfo,
+              let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+
+        // Calculate the inset of the scroll view
+        let keyboardHeight = keyboardFrame.height
+
+        // Set the content inset for the scroll view
+        var contentInset = scrollView.contentInset
+        contentInset.bottom = keyboardHeight
+        scrollView.contentInset = contentInset
+
+        // Adjust the scroll indicator inset
+        scrollView.scrollIndicatorInsets = contentInset
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        // Reset the content inset when the keyboard hides
+        var contentInset = scrollView.contentInset
+        contentInset.bottom = 0
+        scrollView.contentInset = contentInset
+
+        // Reset the scroll indicator inset
+        scrollView.scrollIndicatorInsets = contentInset
     }
 
     // A convenience method to instantiate from the storyboard
