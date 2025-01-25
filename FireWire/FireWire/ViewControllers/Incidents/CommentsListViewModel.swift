ae2a26633e97828a135f4fe76f258d4fc51f3a0d
Sujitha <atom.sujitha@gmail.com>
2025-01-25 18:16:35 +0530

    Add pagination to comment list

--- FireWire/FireWire/ViewControllers/Incidents/CommentsListViewModel.swift
+++ FireWire/FireWire/ViewControllers/Incidents/CommentsListViewModel.swift
@@ -7,16 +7,22 @@
 
 import Foundation
 
-public final class CommentsListViewModel {
-    var commentsList: [CommentsData] = []
-    var delegate: CommentsListViewDelegate?
+public final class CommentsListViewModel: PaginatableViewModel {
+    typealias DataType = CommentsData
 
-    func getCommentsList(for incidentID: String) {
-        let requestURL = String.init(format: APIEndpoints.commentsList, incidentID)
+    var currentPage: Int = 1
+    var totalPages: Int = 1
+    var limit: Int = 10
+    var items: [CommentsData] = []
+    var delegate: CommentsListViewDelegate?
+    var selectedIncidentID: String?
+
+    func fetchData(forPage page: Int, completion: @escaping (Result<[CommentsData], any Error>) -> Void) {
+        guard let selectedIncidentID else { return }
+        let requestURL = String(format: APIEndpoints.commentsList, selectedIncidentID)
 
         let requestModel = IncidentLocalityRequestModel(sortBy: "createdAt", sortDir: "desc", offset: 1, limit: 10)
         let getCommentsRequestModel = APIPayload.incidentLocalityList(requestModel).toDictionary()
-
 
         APIRequest().callApi(
             apiEndPoint: requestURL,
@@ -31,32 +37,53 @@ public final class CommentsListViewModel {
             }
 
             if let commentsResponse = apiResponse as? CommentsResponseModel {
-                self?.commentsList = commentsResponse.data.data
+                let newItems = commentsResponse.data.data
+                self?.totalPages = commentsResponse.data.pageInfo.totalCount
                 DispatchQueue.main.async {
-                    self?.delegate?.dataReceived()
+                    completion(.success(newItems))
                 }
-            }else{
-                print("Invalid response object")
+            } else {
+                completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Failed to parse response"])))
             }
         }
     }
 
-    func addComment(_ model: AddCommentRequestModel){
+    func didFetchData(_ data: [CommentsData]) {
+        items.append(contentsOf: data) // Append new items to existing list
+        delegate?.dataReceived()
+    }
+
+    func getCommentsList(for incidentID: String) {
+        selectedIncidentID = incidentID
+        fetchData(forPage: currentPage) { [weak self] result in
+            switch result {
+            case .success(let newItems):
+                self?.didFetchData(newItems)
+            case .failure(let error):
+                print("Error fetching incidents: \(error)")
+            }
+        }
+    }
+
+    func addComment(_ model: AddCommentRequestModel) {
         APIRequest().callApi(
             apiEndPoint: APIEndpoints.addComment,
             payload: APIPayload.addComment(model).toDictionary(),
             expect: SuccessResponseModel.self)
-        {[weak self] response, _, _ in
+        { [weak self] response, _, _ in
 
             guard let apiResponse = response else {
                 return
             }
 
             if apiResponse is SuccessResponseModel {
-                DispatchQueue.main.async {
-                    self?.delegate?.commentAdded()
+                if let incidentId = self?.selectedIncidentID {
+                    // Reset the items list and append new items
+                    self?.currentPage = 1
+                    self?.items.removeAll()
+                    self?.getCommentsList(for: incidentId)
                 }
-            }else{
+            } else {
                 print("Invalid response object")
             }
         }
--- FireWire/FireWire/ViewControllers/Incidents/CommentsViewController.swift
+++ FireWire/FireWire/ViewControllers/Incidents/CommentsViewController.swift
@@ -26,6 +26,7 @@ class CommentsViewController: UIViewController, CommentsListViewDelegate, UIText
     var coordinator: IncidentsCoordinator?
     var viewModel: CommentsListViewModel!
     var attachedImages: [String] = []
+    var paginationHandler: PaginationHandler<CommentsListViewModel>!
 
     private var selectedIncidentID: String?
 
@@ -62,6 +63,8 @@ class CommentsViewController: UIViewController, CommentsListViewDelegate, UIText
         selectedIncidentID = id
         viewModel = CommentsListViewModel()
         viewModel?.delegate = self
+
+        paginationHandler = PaginationHandler(viewModel: viewModel)
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -82,7 +85,7 @@ class CommentsViewController: UIViewController, CommentsListViewDelegate, UIText
         commentsListCount.isHidden = false
         noCommentsLabel.isHidden = true
 
-        commentsListCount.text = "\(viewModel.commentsList.count) Comments"
+        commentsListCount.text = "\(viewModel.items.count) Comments"
         tableView.reloadData()
     }
 
@@ -95,6 +98,7 @@ class CommentsViewController: UIViewController, CommentsListViewDelegate, UIText
 
     func commentAdded() {
         if let selectedIncidentID {
+            viewModel.currentPage = 1
             viewModel?.getCommentsList(for: selectedIncidentID)
         }
     }
@@ -174,6 +178,17 @@ class CommentsViewController: UIViewController, CommentsListViewDelegate, UIText
         scrollView.scrollIndicatorInsets = contentInset
     }
 
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let contentHeight = scrollView.contentSize.height
+        let scrollOffset = scrollView.contentOffset.y
+        let screenHeight = scrollView.frame.size.height
+
+        if contentHeight - scrollOffset <= screenHeight {
+            // Load the next page when the user scrolls to the bottom
+            paginationHandler.loadNextPage()
+        }
+    }
+    
     static func instantiate() -> CommentsViewController {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let viewController = storyboard.instantiateViewController(withIdentifier: "CommentsViewController") as! CommentsViewController
@@ -183,12 +198,12 @@ class CommentsViewController: UIViewController, CommentsListViewDelegate, UIText
 
 extension CommentsViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        viewModel.commentsList.count
+        viewModel.items.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: CommentsListViewCell.identifier, for: indexPath) as! CommentsListViewCell
-        cell.setupView(viewModel.commentsList[indexPath.row])
+        cell.setupView(viewModel.items[indexPath.row])
         return cell
     }
 }
