cc10361f29cf1b9a9c614b767af8433cd822735f
Sujitha <atom.sujitha@gmail.com>
2025-01-30 21:23:56 +0530

    Remove repeating function

--- FireWire/FireWire/ViewControllers/Incidents/IncidentListViewController.swift
+++ FireWire/FireWire/ViewControllers/Incidents/IncidentListViewController.swift
@@ -67,12 +67,6 @@ class IncidentListViewController: UIViewController, PostListViewDelegate {
         noIncidentsLabel.isHidden = false
     }
 
-    func noIncidentData() {
-        showActivityIndicator(false)
-        tableView.isHidden = true
-        noIncidentsLabel.isHidden = false
-    }
-
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let contentHeight = scrollView.contentSize.height
         let scrollOffset = scrollView.contentOffset.y
