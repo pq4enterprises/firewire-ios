ec4f987fc90fd461ae1ceaefa80cf78e1b44f764
Sujitha <atom.sujitha@gmail.com>
2025-02-09 19:26:01 +0530

    Optimised code for home screen

---
+++ FireWire/FireWire/ViewControllers/Home/IncidentsViewController.swift
@@ -0,0 +1,161 @@
+//
+//  IncidentsViewController.swift
+//  FireWire
+//
+//  Created by Sujitha Palanisamy on 09/02/25.
+//
+
+import GoogleMaps
+import UIKit
+
+protocol IncidentsViewViewDelegate: AnyObject {
+    func incidentDataLoaded()
+    func noIncidentData()
+    func error(message: String)
+}
+
+class IncidentsViewController: UIViewController {
+    @IBOutlet weak var mapHConstraint: NSLayoutConstraint!
+    @IBOutlet weak var incidentContainerView: UIView!
+    @IBOutlet weak var mapContentView: UIView!
+    @IBOutlet weak var incidentTableView: UITableView!
+
+    var coordinator: HomeCoordinator?
+    var appCoordinator: AppCoordinator?
+    var incidentsViewModel: IncidentsViewModel!
+    var panGestureRecognizer: UIPanGestureRecognizer!
+    var paginationHandler: PaginationHandler<IncidentsViewModel>!
+
+    var mapView: GMSMapView!
+    var mapManager: MapManager!
+
+    var incidentView: IncidentListViewController!
+    
+    var initialMapViewHeight: CGFloat = 500
+    var minMapViewHeight: CGFloat = 60
+
+    init(viewModel: IncidentsViewModel) {
+        self.incidentsViewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+        incidentsViewModel.delegate = self
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        incidentsViewModel.getIncidentList()
+        paginationHandler = PaginationHandler(viewModel: incidentsViewModel)
+        setupUI()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        panGestureRecognizer.isEnabled = true
+        incidentTableView.isScrollEnabled = false
+    }
+
+    func setupUI() {
+        mapManager = MapManager()
+        mapView = mapManager.setupMapView(frame: mapContentView.bounds)
+        mapContentView.addSubview(mapView)
+
+        incidentTableView.dataSource = self
+        incidentTableView.delegate = self
+
+        panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture(_:)))
+        incidentTableView.addGestureRecognizer(panGestureRecognizer)
+
+        incidentTableView.register(IncidentListViewCell.nib(), forCellReuseIdentifier: IncidentListViewCell.identifier)
+    }
+
+    func loadIncidentList(){
+        if incidentsViewModel.items.count > 0 {
+            //noIncidentsLabel.isHidden = true
+            incidentTableView.isHidden = false
+            incidentTableView.reloadData()
+        }else{
+            incidentTableView.isHidden = true
+            //noIncidentsLabel.isHidden = false
+        }
+    }
+
+    func addMapMarkers() {
+        mapManager.addMarkers(coordinates: incidentsViewModel.markersList)
+
+        // focus on first marker
+        guard !incidentsViewModel.markersList.isEmpty else { return }
+        let firstLocation = incidentsViewModel.markersList[0]
+        let camera = GMSCameraPosition.camera(withTarget: firstLocation, zoom: 15.0)
+        mapView.animate(to: camera)
+    }
+
+    @objc func handlePanGesture(_ gesture: UIPanGestureRecognizer) {
+        // Get the translation of the gesture (distance the user has moved their finger)
+        let translation = gesture.translation(in: incidentTableView)
+
+        let newHeight = initialMapViewHeight + translation.y
+        let finalHeight = max(newHeight, minMapViewHeight)
+
+        mapHConstraint.constant = finalHeight
+
+        if mapHConstraint.constant == minMapViewHeight {
+            gesture.isEnabled = false
+            incidentTableView.isScrollEnabled = true
+        }
+
+        if gesture.state == .ended || gesture.state == .cancelled {
+            initialMapViewHeight = finalHeight
+        }
+    }
+}
+
+extension IncidentsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        incidentsViewModel.items.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: IncidentListViewCell.identifier, for: indexPath) as! IncidentListViewCell
+
+        let selectedIncident = incidentsViewModel.items[indexPath.row]
+
+        cell.favAction = {
+            let value = selectedIncident.likeCount > 0 ? false : true
+            self.incidentsViewModel.favouriteIncident(incidentId: selectedIncident.id, like: value) { result in
+                if result {
+                    tableView.reloadData()
+                }
+            }
+        }
+        cell.shareAction = {
+            self.showToast(message: "share action")
+        }
+        cell.setupView(selectedIncident)
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        coordinator?.dismissView(animated: true)
+        let selectedIncidentID = incidentsViewModel.items[indexPath.row].id
+        //coordinator?.navigateToIncidentDetail(selectedIncidentID)
+    }
+}
+
+extension IncidentsViewController: IncidentsViewViewDelegate{
+    func incidentDataLoaded() {
+        loadIncidentList()
+        addMapMarkers()
+    }
+
+    func noIncidentData() {
+
+    }
+
+    func error(message: String) {
+        
+    }
+}
---
+++ FireWire/FireWire/ViewControllers/Home/IncidentsViewModel.swift
@@ -0,0 +1,117 @@
+//
+//  IncidentsViewModel.swift
+//  FireWire
+//
+//  Created by Sujitha Palanisamy on 09/02/25.
+//
+
+import Foundation
+import MapKit
+
+final class IncidentsViewModel: PaginatableViewModel {
+    typealias DataType = IncidentDataModel
+
+    var currentPage: Int = 1
+    var totalPages: Int = 1
+    var limit: Int = 10
+    var items: [IncidentDataModel] = []
+    var markersList: [CLLocationCoordinate2D] = []
+
+    var selectedLocalities: SelectedLocalities?
+    var delegate: IncidentsViewViewDelegate?
+
+    init(_ incidentList: [IncidentDataModel] = [], _ selectedLocalities: SelectedLocalities? = nil) {
+        self.selectedLocalities = selectedLocalities
+        getIncidentList(selectedLocalities: selectedLocalities)
+    }
+
+    func fetchData(forPage page: Int, completion: @escaping (Result<[IncidentDataModel], any Error>) -> Void) {
+        var requestModel = IncidentRequestModel(sortBy: "createdAt", sortDir: "desc", offset: page, limit: limit)
+
+        // Add filters if available
+        if let selectedLocalities = selectedLocalities {
+            requestModel.query = QueryModel(
+                locality: selectedLocalities.selectedLocalityIDs,
+                subLocality: selectedLocalities.selectedSubLocalityIDs
+            )
+        }
+
+        let getIncidentRequestModel = APIPayload.incidentList(requestModel).toDictionary()
+
+        // Perform API call
+        APIRequest().callApi(
+            apiEndPoint: APIEndpoints.incidentList,
+            payload: getIncidentRequestModel,
+            expect: IncidentResponseModel.self,
+            requestType: APIConstants.GET
+        ) { [weak self] response, _, _ in
+            guard let apiResponse = response else {
+                self?.delegate?.noIncidentData()
+                return
+            }
+
+            if let incidentListResponse = apiResponse as? IncidentResponseModel {
+                let newItems = incidentListResponse.data.data
+                self?.totalPages = incidentListResponse.data.pageInfo.totalCount
+                completion(.success(newItems))
+            } else {
+                completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Failed to parse response"])))
+            }
+        }
+    }
+
+    func didFetchData(_ data: [IncidentDataModel]) {
+        items.append(contentsOf: data) // Append new items to existing list
+
+        // for map markers
+        for item in data {
+            if let lat = Double(item.latitude), let lon = Double(item.longitude) {
+                let coordinates = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+                self.markersList.append(coordinates)
+            }
+        }
+
+        delegate?.incidentDataLoaded()
+    }
+
+    // Public method to trigger data fetching
+    func getIncidentList(selectedLocalities: SelectedLocalities? = nil) {
+        fetchData(forPage: currentPage) { [weak self] result in
+            switch result {
+            case .success(let newItems):
+                self?.didFetchData(newItems)
+            case .failure(let error):
+                print("Error fetching incidents: \(error)")
+            }
+        }
+    }
+
+    func favouriteIncident(incidentId: String, like: Bool, completion: @escaping (Bool) -> Void){
+
+        let requestModel = APIPayload.favouriteIncident(
+            userId: UserDefaults.standard.string(forKey: "user_id") ?? "",
+            incidentId: incidentId,
+            type: like == true ? "like" : "unlike"
+        ).toDictionary()
+
+        APIRequest().callApi(
+            apiEndPoint: APIEndpoints.favIncident,
+            payload: requestModel as JSON,
+            expect: SuccessResponseModel.self)
+        {response, _, _ in
+
+            guard let apiResponse = response as? SuccessResponseModel else {
+                let errorMessage = (response == nil) ? "Invalid response" : "Unexpected response format"
+                self.delegate?.error(message: errorMessage)
+                return
+            }
+
+            if apiResponse.code != "success" {
+                self.delegate?.error(message: apiResponse.message)
+            }else{
+                completion(true)
+            }
+
+        }
+    }
+}
